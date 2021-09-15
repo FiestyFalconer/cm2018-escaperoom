9f591302ffe30a0a3d55ab4347ba963004eedfd1
FiestyFalconer <60130986+FiestyFalconer@users.noreply.github.com>
2021-09-15 16:14:03 +0200

    function getEnigmeCode

--- binary/php/pdo.php
+++ binary/php/pdo.php
@@ -7,8 +7,24 @@
   Version     : 1.0
 */
 
+require_once "database.php";
 
+function getEnigmeCode(){
+  try{
+    $today = date("Y-m-d");
 
+    $query = getConnexion()->prepare("
+      SELECT `solution`.`en1`, `solution`.`en2`, `solution`.`en3`
+      FORM `escapegame_2021`.`solution`
+      WHERE ``solution`.`jour` = ?
+    ");
+    $query->execute([$today]);
+    return $query->fetchAll(PDO::FETCH_ASSOC);
+  }
+  catch(PDOException $e){
+    echo 'Exception reçue : ',  $e->getMessage(), "\n";
+  }
+}
 
 /*
 // Méthode qui permet de savoir le nombre de partie différente
