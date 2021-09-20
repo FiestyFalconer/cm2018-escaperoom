e1dbdca0c43580ffa681fcfb34306bfdf66bd22c
LethalDarkLight <darkmihawk1290@gmail.com>
2021-09-20 10:34:58 +0200

    amélioration page login
    boucle optimisé, script js séparé

--- binary/login.php
+++ binary/login.php
@@ -3,17 +3,11 @@ function affichageButtons()
 {
     $chaine = '<div class="btnContainer">';
 
-    for ($i = 0; $i < 10; $i++) {
-        $chaine .= "<input value=\"$i\" type=\"Button\" onclick=\"btnClick($i)\" id=\"$i\" class=\"btn btn-lg btn-primary\" />";
+    for ($i = 0; $i <= 15; $i++)
+    {
+        $hexa = strtoupper(dechex($i));
+        $chaine .= "<input value=\"$hexa\" type=\"Button\" onclick=\"btnClick($hexa)\" id=\"$hexa\" class=\"btn btn-primary\" />";
     }
-    $chaine .= '
-            <input value="A" type="Button" onclick="btnClick("A")" id="A" class="btn btn-lg btn-primary"/>
-            <input value="B" type="Button" onclick="btnClick("B")" id="B" class="btn btn-lg btn-primary"/>
-            <input value="C" type="Button" onclick="btnClick("C")" id="C" class="btn btn-lg btn-primary"/>
-            <input value="D" type="Button" onclick="btnClick("D")" id="D" class="btn btn-lg btn-primary"/>
-            <input value="E" type="Button" onclick="btnClick("E")" id="E" class="btn btn-lg btn-primary"/>
-            <input value="F" type="Button" onclick="btnClick("F")" id="F" class="btn btn-lg btn-primary"/>
-    ';
     return $chaine;
 }
 ?>
@@ -23,16 +17,16 @@ function affichageButtons()
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
     
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/login.css">
 
-    <script src="js/binary.js"></script>
+    <script src="js/login.js"></script>
 </head>
 
 <body>
-    <h1>LOGIN</h1>
+    <h1>Saisissez le code secret</h1>
     <p>
         <span id="valueEn1">_</span>
         &nbsp;
