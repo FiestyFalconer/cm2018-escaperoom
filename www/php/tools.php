c910020fb6a29c136b5d2ee1be69b3810b0b5119
FiestyFalconer <60130986+FiestyFalconer@users.noreply.github.com>
2021-10-13 11:38:18 +0200

    tools function

--- www/php/tools.php
+++ www/php/tools.php
@@ -19,3 +19,33 @@ function showButtons()
     }
     return $chaine;
 }
+
+////////////////////////////////////////////////////////////////////////
+function showDash($carreDeDeux){
+  $string = " <div class='row'>";
+
+  $string .="    <p class='carreDeDeux'>
+                  &nbsp;2<sup>3</sup>&nbsp;
+                  &nbsp;2<sup>2</sup>&nbsp;
+                  &nbsp;2<sup>1</sup>&nbsp;
+                  &nbsp;2<sup>0</sup>&nbsp;
+                </p>
+              </div>
+              <div class='row binary'>
+                 <hr>";
+    
+  if($carreDeDeux){
+    for ($i = 0; $i < 4; $i++){
+      $string .= '&nbsp;<span id="value' . $i . '">_</span>&nbsp;';
+    };
+  }
+  else{
+    for ($i = 4; $i < 8; $i++){
+      $string .= '&nbsp;<span id="value' . $i . '">_</span>&nbsp;';
+    };          
+  }
+  
+  $string .=" <hr>
+            </div>";
+  return $string;            
+}
